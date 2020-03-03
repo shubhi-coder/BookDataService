dd0aa096f90728246bf849b31821c977ace47eef
shubhi-coder <61703802+shubhi-coder@users.noreply.github.com>
2020-03-03 21:05:44 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookDataService.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookDataService
+{
+    public class Startup
+    {
+        public IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContextPool<DbContextBookData>(options => options.UseSqlServer(_config.GetConnectionString("BookDataServicesConnection")));
+            services.AddMvc().AddXmlSerializerFormatters();
+            services.AddScoped<IBookDataRepository, SQLBookDataServiceRepository>();//MockBookDataServiceRepository
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger
+            )
+        {
+            if (env.IsDevelopment())
+            {
+                DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions();
+                app.UseDeveloperExceptionPage(developerExceptionPageOptions);
+            }
+            app.UseMvcWithDefaultRoute();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
