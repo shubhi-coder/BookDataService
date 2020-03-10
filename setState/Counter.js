c7faa691d00a4b1411d45d88025a5e8c1a27d199
shubhi-coder <61703802+shubhi-coder@users.noreply.github.com>
2020-03-10 16:32:48 +0530

    Add files via upload
    Incrementing 5 times

--- setState/Counter.js
+++ setState/Counter.js
@@ -13,20 +13,36 @@ class Counter extends Component {
     increment()
     {
         // this.state.count=this.state.count+1 this will only increment cntr in the logs
-        this.setState(
-            {
-            count: this.state.count+1
-        },
-         ()=>{console.log('Callback value', this.state.count)
-        }
-        )
+        // this.setState(
+        //     {
+        //     count: this.state.count+1
+        // },
+        //  ()=>{console.log('Callback value', this.state.count)
+        // }
+        // )
         //console.log(this.state.count)
+
+        //in order to use incrementfive() method
+        this.setState(prevState=>({
+            count: prevState.count + 1
+        }))
+        console.log(this.state.count)
     }
+
+    incrementfive()
+    {
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+    }
+
     render() {
         return (
             <div>
                 Count-{this.state.count}
-                <button onClick={()=>this.increment()}>Increment</button>
+                <button onClick={()=>this.incrementfive()}>Increment</button>
             </div>
         );
     }
